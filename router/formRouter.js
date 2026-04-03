0c7e28cae170498503d33b02631a41c826b4c6af
ManatThakur <manatthakur7@gmail.com>
2026-04-03 18:50:01 +0530

    added new features

--- router/formRouter.js
+++ router/formRouter.js
@@ -2,17 +2,25 @@ const express=require('express');
 const path = require('path');
 const rootdir=require('../util/path');
 const formRouter=express.Router();
-
+const detail=[];
 const bodyParser = require('body-parser');
 formRouter.use(bodyParser.urlencoded({ extended: true }));
 formRouter.get("/form",(req,res,next)=>{
   console.log("Hello world route accessed",req.method,req.url,req.body);
   res.sendFile(path.join(rootdir, 'view', 'form.html'));
- 
+ //res.redirect('/'); not here bcoz it redirect page to home
 });
 formRouter.post("/form",(req,res,next)=>{
   console.log("2nd route accessed",req.method,req.url,req.body);
- res.send(`Form submitted. Name: ${req.body.homeName}, Address: ${req.body.address}, Price: ${req.body.price}, Rooms: ${req.body.rooms}`);
-
+ res.send(`Form submitted. Name: ${req.body.homeName}, Address: ${req.body.address}, Price: ${req.body.price}, Rooms: ${req.body.rooms}, Image: ${req.body.image}`);
+ detail.push({
+    homeName: req.body.homeName,
+    address: req.body.address,
+    price: req.body.price,
+    rooms: req.body.rooms,
+    image: req.body.image
+  });
+  res.redirect('/');
 });
-module.exports=formRouter;
+exports.detail=detail;
+exports.router=formRouter;
--- router/homerouter.js
+++ router/homerouter.js
@@ -2,8 +2,15 @@ const express=require('express');
 const homeRouter=express.Router();
 const path = require('path');
 const rootdir=require('../util/path');
+
+const {detail}=require('./formRouter');
+
+
+
 homeRouter.get('/', (req, res) => {
- 
-res.sendFile(path.join(rootdir, 'view', 'rename.html'));
+	//res.sendFile(path.join(rootdir, 'view', 'rename.html'));
+	res.render('rename',{homes:detail});
+	res.end();
 });
+
 module.exports=homeRouter;
